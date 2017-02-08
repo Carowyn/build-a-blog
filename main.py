06e998908bc73893ac26eaafca475252aeef2b42
Brittani <brittaniluce@gmail.com>
2017-02-07 23:48:03 -0600

    initial commit.

--- main.py
+++ main.py
@@ -15,11 +15,70 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class NewPost(db.Model):
+    title = db.StringProperty(required = True)
+    post_text = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("/blog")
+
+class ViewPostHandler(Handler):
+    def render_front(self, title="", post_text=""):
+        posts = db.GqlQuery("SELECT * FROM NewPost ORDER BY created DESC LIMIT 5 ")
+        self.render("front.html", title=title, post_text=post_text, posts=posts)
+
+    def get(self, id):
+        if id:
+            single_post = Post.get_by_id(id)
+            self.render("single.html")
+###ASK HOW TO MAKE THIS WORK IN SLACK TOMORROW GRRRRRRRRR
+        else:
+            self.render_front()
+
+class NewPostHandler(Handler):
+    def render_new(self, title="", post_text="", error=""):
+        self.render("newpost.html", title=title, post_text=post_text, error=error)
+
+    def get(self):
+        self.render_new()
+
+    def post(self):
+        title = self.request.get("title")
+        post_text = self.request.get("post_text")
+
+        if title and post_text:
+            full_post = NewPost(title=title, post_text=post_text)
+            full_post.put()
+
+            self.redirect("/blog")
+
+        else:
+            error = "We need both a title and a post!"
+            self.render_new(title, post_text, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler),
+    ('/newpost', NewPostHandler)
 ], debug=True)
